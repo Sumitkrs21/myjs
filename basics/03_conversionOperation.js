124278a8fa90089d77cc9b7d4eadee5a96c519be
Sumit <145886518+Sumitkrs21@users.noreply.github.com>
2025-06-10 19:11:02 +0000

    conversion of datatype

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,36 @@
+let score = "sumit";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33
+// "33abs" => NaN
+//"true" => 1
+//"false"=> 0
+//null => 0  type=object
+//undefined =>NaN
+
+let isLoggedIn = "sumit";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// 1=> true
+// 0=> false
+// "" => false
+// "sumit" => true
+
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
+
+// 33=> 33 type=string
+
+
