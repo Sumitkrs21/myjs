acdb5c1f01bffda3dad7f5f46b6f8f8d5d109bd6
Sumit <145886518+Sumitkrs21@users.noreply.github.com>
2025-06-12 21:30:03 +0000

    comparsion and operations

---
+++ basics/04_comparision.js
@@ -0,0 +1,31 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null>=0);
+// console.log(null==0);
+//comparison mai null zero mai convert hota hai 
+
+// console.log(undefined==0);
+// console.log(undefined>=0);
+// console.log(undefined>0);
+// but undefined nahi hota hai zero mai convert 
+
+// === strict checking (check values as well as datatype)
+
+console.log("2"=== 2);
+
+
+
+
+
+
+
+
