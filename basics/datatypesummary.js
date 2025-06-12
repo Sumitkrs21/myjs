a57d09ce673a5b21377f27ca6b641dcf8fb677be
Sumit <145886518+Sumitkrs21@users.noreply.github.com>
2025-06-12 22:10:17 +0000

    datatypesummary

---
+++ basics/datatypesummary.js
@@ -0,0 +1,35 @@
+ // Primitive
+
+ // 7 types: String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score = 100
+ const scoreValue = 100.78
+const isLoggedIn = false 
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 213456789034567n
+
+ //Reference (Non primitve)
+
+ // Array, Object, Functions
+
+
+ const heros = ["sumti","tapan","manish","vanshaj"]
+  let myObj={
+    name:"sumit",
+    age:20
+
+  }
+
+ const myFunction= function(){
+    console.log("hello wolrd ");
+    
+ }
+ console.log(typeof id);
+ 
