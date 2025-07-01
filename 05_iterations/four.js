3b45a3ea629a12828c001d6e28b4daa884292897
Sumit <145886518+Sumitkrs21@users.noreply.github.com>
2025-07-01 23:35:36 +0000

    for in loop (return key)

---
+++ 05_iterations/four.js
@@ -0,0 +1,29 @@
+const myObject={
+    Js:"javascript",
+    Cpp:"c++",
+    Rb:"ruby",
+    Swift:"swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} is shortcut for ${myObject[key]}`);
+    
+}
+
+const programming=['js','cpp','py','java','rb']
+for (const key in programming) {// return the in default mode
+    //console.log(programming[key]);
+}
+
+//Map
+// const map = new Map()
+// map.set("JH",'Jharkhand')
+// map.set('DL','Delhi')
+// map.set('UP','UttarPradesh')
+// //console.log(map);
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
+
